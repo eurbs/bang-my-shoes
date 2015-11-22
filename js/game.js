4f3adfa8ea69b60290f2b7e8d816b62f9adfc700
nconfrey <nickconfrey@earthlink.net>
2015-11-22 10:05:12 -0600

    switched to textmesh word rendering

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ function chooseRandomLocation()
 {
 	//number between 1 - 7
 	var rand = Math.floor(((Math.random()*6) + 1));
-	var loc = { city: location_dictionary[rand][0], lat: location_dictionary[rand][2], lng: location_dictionary[rand][3]};
+	var loc = { country: location_dictionary[rand][1], city: location_dictionary[rand][0], lat: location_dictionary[rand][2], lng: location_dictionary[rand][3]};
 	curWin = location_dictionary[rand][0];
 
 	//play associated song
