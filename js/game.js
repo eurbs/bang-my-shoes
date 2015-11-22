490021a94f1aba9c7927cd95d56f386033eb5518
nconfrey <nickconfrey@earthlink.net>
2015-11-22 03:26:53 -0600

    unstoppable music and fixed australia

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
 
-//City, Country, lat, lon
+//City, Country, lat, lon, soundcloud track number
 var location_dictionary=[
-["Paris", "France", 48.865023, 2.328205],
-["Dublin", "Ireland", 53.342534, -6.272],
-["Boston", "USA", 42.3599492,-71.0557988],
-["San Francisco", "USA", 37.8039454,-122.4777254],
-["Sydney", "Australia", -33.8590028,151.2128384],
-["Nirobi", "Kenya", -1.3765767,36.7744464]
+["Paris", "France", 48.865023, 2.328205, 120056951],
+["Dublin", "Ireland", 53.342534, -6.272, 174090940],
+["Boston", "USA", 42.3599492,-71.0557988, 120163885],
+["San Francisco", "USA", 37.8039454,-122.4777254, 20615012],
+["Sydney", "Australia", -33.858008,151.214097, 117929630],
+["Nirobi", "Kenya", -1.3765767,36.7744464, 179592869]
 ]
 
 var score = 600;
@@ -14,7 +14,10 @@ var scoreTimer = setInterval(scoreClock, 1000);
 
 function scoreClock() {
     score -= 5;
+    console.log(score);
 }
+
+function getScore(){return score;}
 
 function stopScore() {
 	var temp = score;
@@ -28,5 +31,9 @@ function chooseRandomLocation()
 	//number between 1 - 7
 	var rand = Math.floor(((Math.random()*6) + 1));
 	var loc = { lat: location_dictionary[rand][2], lng: location_dictionary[rand][3]};
+	
+	//play associated song
+	streamSound(location_dictionary[rand][4]);
+
 	return loc;
 }
